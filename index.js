ac59a1db225699b663007c098b0294aa0a235ecc
Dailey Kaze <daileykaze@gmail.com>
2020-10-14 19:03:36 -0400

    Added prompts and fs

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+
 // array of questions for user
 const questions = [
-
+    {
+        message:"What is the title of your project?",
+        name: "projectTitle"
+    },
+    {
+        message: "Provide a brief description of your project: ",
+        name: "projectDesc"
+    },
+    {
+        message: "What technologies did you use?",
+        name: "tech"
+    },
+    // {
+    //     message: "What are the install requirements?",
+    //     name: "install"
+    // },
+    {
+        message: "Which License would you like to use",
+        name: "licenseType"
+    },
+    // {
+    //     message:"Do you have any contributors?",
+    //     name: "teamMembers"
+    // },
+    // {
+    //     message: "Provide the tests conducted: ",
+    //     name: "tests"
+    // },
+    {
+        message: "Provide the source for a screenshot of the product: ",
+        name: "imgSource"
+    },
 ];
 
 // function to write README file
@@ -8,8 +42,29 @@ function writeToFile(fileName, data) {
 }
 
 // function to initialize program
-function init() {
+async function init() {
+    const projectSetup = await inquirer.prompt(questions)
+    
+    // write to file
+    fs.writeFileSync('sample.md', 
+    `# ${projectSetup.projectTitle}
 
+## Description
+${projectSetup.projectDesc}
+
+## Technologies
+${projectSetup.tech}
+
+## Usage
+The following image is an example of the web application's appearance and functionality: 
+![Image](${projectSetup.imgSource})
+
+## License
+${projectSetup.licenseType}
+    `)
+
+    const checkOutput = fs.readFileSync('sample.md', 'utf8')
+    console.log('The final sample.md: ', checkOutput)
 }
 
 // function call to initialize program
