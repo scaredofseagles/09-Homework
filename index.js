21be16f5c9789928f028c915b540977d9453df5a
Dailey Kaze <daileykaze@gmail.com>
2020-10-14 21:05:57 -0400

    Completed file writing logic

--- index.js
+++ index.js
@@ -12,13 +12,21 @@ const questions = [
         name: "projectDesc"
     },
     {
-        message: "What technologies did you use?",
+        message: "How many technologies did you use?",
         name: "tech"
     },
-    // {
-    //     message: "What are the install requirements?",
-    //     name: "install"
-    // },
+    {
+        message: "What are the install requirements?",
+        name: "install"
+    },
+    {
+        message: "Insert a snippet of your code here",
+        name: "codeSnippet"
+    },
+    {
+        message: "Give your code snipper a title",
+        name: "codeSnippetTitle"
+    },   
     {
         type: "list", 
         message: "Which License would you like to use",
@@ -36,17 +44,26 @@ const questions = [
     {
         message: "Provide the source for a screenshot of the product: ",
         name: "imgSource"
-    },
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
+let techList = []
+
 // function to initialize program
 async function init() {
     const projectSetup = await inquirer.prompt(questions)
     
+    for (var i=0; i<projectSetup.tech; i++){
+        const techInfo = await inquirer.prompt({
+            message: "What technology did you use?",
+            name: "techName"
+        })
+        techList.push(techInfo)
+    }
     // write to file
     fs.writeFileSync('sample.md', 
     `# ${projectSetup.projectTitle}
@@ -54,8 +71,22 @@ async function init() {
 ## Description
 ${projectSetup.projectDesc}
 
-## Technologies
-${projectSetup.tech}
+## Installation
+    ${projectSetup.install}
+
+## Technologies`)
+
+    techList.forEach(function(technology){
+        fs.appendFileSync('sample.md', `
+* ${technology.techName}`)
+    })
+
+    fs.appendFileSync('sample.md', `
+
+## What I Did
+
+#### ${projectSetup.codeSnippetTitle}
+    ${projectSetup.codeSnippet}
 
 ## Usage
 The following image is an example of the web application's appearance and functionality: 
@@ -63,6 +94,11 @@ The following image is an example of the web application's appearance and functi
 
 ## License
 ${projectSetup.licenseType}
+
+============================================================
+
+This README was created using a README Generator. See the Repo: [github.com/scaredofseagles/README-Generator](https://github.com/scaredofseagles/README-Generator)
+
     `)
 
     const checkOutput = fs.readFileSync('sample.md', 'utf8')
